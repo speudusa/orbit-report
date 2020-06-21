12462d77c9a3a34a3b84f89e65d368822a233d43
speudusa <66076696+speudusa@users.noreply.github.com>
2020-06-21 15:06:50 -0500

    updated orbit report table.  completed step 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Satellite } from './satellite';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
