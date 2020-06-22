d3e3e2dd9e9ab02fd6b2f038a9df83c9fd434746
speudusa <66076696+speudusa@users.noreply.github.com>
2020-06-22 13:36:02 -0500

    funtion working.  part 6 complete.

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -3,17 +3,22 @@ export class Satellite {
     orbitType: string; 
     type: string; 
     operational: boolean; 
-    launchDate: string;
-
+    launchDate: string; 
+        
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) 
     {
         this.name = name; 
         this.type = type; 
         this.launchDate = launchDate;
         this.orbitType = orbitType; 
-        this.operational = operational; 
-    }
-    
-        
-      
+        this.operational = operational;
+               
+    }   
+    shouldShowWarning(): boolean {   //look in chapter 30.4??
+        let changeColor = this.type.toLowerCase();
+        if (changeColor === "space debris") {
+            return true;
+        } 
+        return false;
+    }       
 }
