90c1adbd34ce8e82a795ed1b074940a9e021f2d5
speudusa <66076696+speudusa@users.noreply.github.com>
2020-06-22 16:32:25 -0500

    completed.  search feature works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,14 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite [];
+  
   
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
-
+    
     window.fetch(satellitesUrl).then(function(response) {
       response.json().then(function(data) {
 
@@ -35,7 +38,20 @@ export class AppComponent {
             this.sourceList.push(satellite);
 
         }
+        this.displayList = this.sourceList.slice(0);
       }.bind(this));
     }.bind(this));
   }
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    this.displayList = matchingSatellites;
+ }
+
 }
--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -10,7 +10,18 @@ export class OrbitListComponent implements OnInit {
   @Input() satellites: Satellite[];
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    
   }
 
+  sort(column: string): void {
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
+ }
 }
